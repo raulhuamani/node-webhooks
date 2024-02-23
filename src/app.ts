570a385a3a773388cf7e3b9337d899d1548f3072
raulhuamani <raul.huamani.h@gmail.com>
2024-02-22 23:38:04 -0500

    S24 - WebHooks Server - eguridad por middleware segun documentacion

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { envs } from "./config";
 import { GithubController } from "./presentation/github/controller";
+import { GithubSha256Middleware } from "./presentation/middlewares/github-sh256.middleware";
 
 (() => {
   main();
@@ -12,6 +13,8 @@ function main() {
   const controller = new GithubController();
 
   app.use(express.json());
+
+  app.use(GithubSha256Middleware.verifySignature);
 
   app.post("/api/github", controller.webhookHandler);
 
